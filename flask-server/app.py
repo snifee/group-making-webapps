2c921351fce0f6feb566f8f2853ec54f23dbffc3
snifee <razyaarf@gmail.com>
2021-12-23 04:51:19 +0800

    menambhkan requirement.txt

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,4 +1,3 @@
-from typing import final
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
@@ -32,60 +31,6 @@ def predict():
     result = int(prediction[0])
 
     return jsonify({'value': result})
-# #     output = round(prediction[0], 2)
-
-# #     return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
-
-# # @app.route('/results',methods=['POST'])
-# # def results():
-
-# #     data = request.get_json(force=True)
-# #     prediction = model.predict([np.array(list(data.values()))])
-
-# #     output = prediction[0]
-# #     return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-#================================================================
-# @app.route('/predict', methods=['GET','POST'])
-# def predict():
-    
-#     if request.method == "POST":
-#         #get form data
-#         data = request.json;
-
-#         # print(data);
-        
-#         #call preprocessDataAndPredict and pass inputs
-#         try:
-#             prediction = cluster(data)
-#             #pass prediction to template
-#             return prediction
-   
-#         except ValueError:
-#             return "Please Enter valid values"
-  
-#         pass
-#     pass
-# def cluster(data):
-    
-#     #keep all inputs in array
-#     input = np.array(data)
-
-    
-#     #reshape array
-#     input = input.reshape(1,-1)
-#     print('hehehe')
-#     print(input)
-    
-#     #predict
-#     prediction = model.predict(input)
-    
-#     return prediction
-    
-#     pass
-# if __name__ == '__main__':
-#     app.run(debug=True)
